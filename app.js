2c87bfe5adf5f73563a58449d00ecc5dfc3b77eb
ArlonTuazon <nikka1378@yahoo.com>
2020-12-08 02:03:45 -0800

    started updating html

--- app.js
+++ app.js
@@ -129,19 +129,18 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-
-// const pageHTML = generatePage(name, github);
+     const pageHTML =  generatePage(portfolioData);
 
 
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
+ fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
+
+
 });
