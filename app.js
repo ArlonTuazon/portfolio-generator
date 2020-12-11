25b1cccde8d9b5d09b08d797fdcae3f7aa665f0c
ArlonTuazon <nikka1378@yahoo.com>
2020-12-10 22:51:16 -0800

    start site

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+fs.writeFile('./dist/index.html');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
@@ -133,11 +133,21 @@ promptUser()
 
 
 
- fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+     fs.writeFile('./dist/index.html', pageHTML, err => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log('Page created! Check out index.html in this directory to see it!');
+    
+      fs.copyFile('./src/style.css', './dist/style.css', err => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log('Style sheet copied successfully!');
+      });
+    });
 
 
 
